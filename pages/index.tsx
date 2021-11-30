832aa7b3188855de167e7da042c24cb5569fa4dc
CORP\81665 <davidkim9204@lgcns.com>
2021-11-30 10:40:10 +0900

    fix prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import Head from "next/head";
+import React, { useState } from 'react';
+import Head from 'next/head';
 
 export default function Home() {
-  const [text, setText] = useState<string>("자바스크립트");
+  const [text, setText] = useState<string>('자바스크립트');
 
   setTimeout(() => {
-    setText("타입스크립트");
+    setText('타입스크립트');
   }, 1000);
 
   return (
