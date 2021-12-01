7c3f181de4d48089a8688c172bfe64e4e19649d9
davidkim9204 <davidkim9204@naver.com>
2021-12-01 18:32:49 +0900

    add layout & header nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout/Layout';
 
 export default function Home() {
   const [text, setText] = useState<string>('자바스크립트');
@@ -9,10 +10,13 @@ export default function Home() {
   }, 1000);
 
   return (
-    <div className="container">
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
       <div>
         <span>{text} 적용 완료</span>
       </div>
-    </div>
+    </Layout>
   );
 }
