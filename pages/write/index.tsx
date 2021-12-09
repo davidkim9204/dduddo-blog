afa669c3b4453f9b42e828881f437316c1ae3722
davidkim9204 <davidkim9204@naver.com>
2021-12-09 15:09:12 +0900

    add dynamic import

--- pages/write/index.tsx
+++ pages/write/index.tsx
@@ -1,9 +1,14 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import dynamic from 'next/dynamic';
 import Layout from '../../components/layout/Layout';
 import utilStyles from '../../styles/utils.module.css';
-import ToastEditor from 'components/editor/ToastEditor';
-export default function About() {
+
+const ToastEditor = dynamic(() => import('components/editor/ToastEditor'), {
+  ssr: false,
+});
+
+export default function Write() {
   return (
     <Layout home={false}>
       <Head>
