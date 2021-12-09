7066226f2b3ea39d943754829ea71543b2c849bc
davidkim9204 <davidkim9204@naver.com>
2021-12-09 15:09:23 +0900

    add toast ui temp

--- components/editor/ToastEditor.tsx
+++ components/editor/ToastEditor.tsx
@@ -1,15 +1,42 @@
+import { useRef, useState, createRef } from 'react';
 import '@toast-ui/editor/dist/toastui-editor.css';
-
 import { Editor } from '@toast-ui/react-editor';
 
-const ToastEditor = () => (
-  <Editor
-    initialValue="hello react editor world!"
-    previewStyle="vertical"
-    height="600px"
-    initialEditType="markdown"
-    useCommandShortcut={true}
-  />
-);
+// TOAST UI Editor Plugins
+import chart from '@toast-ui/editor-plugin-chart';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell';
+import uml from '@toast-ui/editor-plugin-uml';
+
+const ToastEditor = () => {
+  const editorRef = createRef<Editor>();
+
+  const btnClickListner = () => {
+    console.log(editorRef);
+    const eI =
+      editorRef && editorRef.current && editorRef.current.getInstance();
+    console.log(eI);
+    const md = eI?.getMarkdown();
+    console.log(md);
+    const hm = eI?.getHtml();
+    console.log(hm);
+  };
+
+  return (
+    <>
+      <Editor
+        initialValue="hello react editor world!"
+        previewStyle="vertical"
+        height="600px"
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+        // plugins={[chart, codeSyntaxHighlight, colorSyntax, tableMergedCell, uml]}
+        ref={editorRef}
+      />
+      <button onClick={btnClickListner}>콘솔에 찍히냐</button>
+    </>
+  );
+};
 
 export default ToastEditor;
