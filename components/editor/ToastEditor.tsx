0826c867524679506d692c529a3fa376f309fe98
davidkim9204 <davidkim9204@lgcns.com>
2022-02-13 15:10:14 +0900

    add editor plugin

--- components/editor/ToastEditor.tsx
+++ components/editor/ToastEditor.tsx
@@ -1,15 +1,17 @@
 import { useRef, useState, createRef } from 'react';
-import dynamic from 'next/dynamic';
-import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import WriteSuccess from 'pages/writeSuccess';
-import Router from 'next/router';
+
+import '@toast-ui/editor/dist/toastui-editor.css';
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
 
 // TOAST UI Editor Plugins
-// import chart from '@toast-ui/editor-plugin-chart';
-// import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
-// import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
-// import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell';
+import chart from '@toast-ui/editor-plugin-chart';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell';
+import uml from '@toast-ui/editor-plugin-uml';
 
 const ToastEditor = () => {
   const editorRef = createRef<Editor>();
@@ -35,7 +37,13 @@ const ToastEditor = () => {
         height="600px"
         initialEditType="wysiwyg"
         useCommandShortcut={true}
-        // plugins={[chart, codeSyntaxHighlight, colorSyntax, tableMergedCell, uml]}
+        plugins={[
+          chart,
+          codeSyntaxHighlight,
+          colorSyntax,
+          tableMergedCell,
+          uml,
+        ]}
         ref={editorRef}
       />
       {editorContent ? (
